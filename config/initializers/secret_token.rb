3ea41e5ecf1eb828c9b8a3ee0d1e73443285c295
Travis <travisR004@gmail.com>
2014-03-19 11:55:33 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SplitMyBills::Application.config.secret_key_base = '7b5205b6012bf7b35b5459a69e13cde8d193a14526cf9cd17e17601b2b9575451d3db516f7267a7a9268e5d02ef80593df3bb60a2225cfbf10ab72770202ebf5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SplitMyBills::Application.config.session_store :cookie_store, key: '_SplitMyBills_session'
