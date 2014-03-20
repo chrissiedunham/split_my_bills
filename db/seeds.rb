63cbf21be0b87ec405459c2feabc96dc53068f2b
Chrissie Deist <chrissie.deist@gmail.com>
2014-03-20 12:07:30 -0700

    Seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+user = User.create([{ email: 'chrissie@gmail.com', name: "Chrissie"},
+                    { email: 'charlie@gmail.com', name: "Charlie"},
+                    { email: 'karen@gmail.com', name: "Karen"}]
+
+friendships = Friendship.create([ { friendship_1_id: 1, friendship_2_id: 2},
+                                  { friendship_1_id: 2, friendship_2_id: 1},
+                                  { friendship_1_id: 1, friendship_2_id: 3},
+                                  { friendship_1_id: 3, friendship_2_id: 1},
+                                  { friendship_1_id: 2, friendship_2_id: 3},
+                                  { friendship_1_id: 3, friendship_2_id: 2} ])
+
+
+
+
