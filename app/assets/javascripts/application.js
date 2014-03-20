10f29d5fac18308edc1280d8b4fbd9dac443f4b2
Chrissie Deist <chrissie.deist@gmail.com>
2014-03-19 17:36:41 -0700

    Add bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require jquery.ui.all
 //= require underscore
+//= require bootstrap
 //= require backbone
 //= require split_my_bills
 //= require_tree ../templates
--- app/assets/javascripts/views/bills/BillsIndex.js
+++ app/assets/javascripts/views/bills/BillsIndex.js
@@ -5,10 +5,27 @@ window.SplitMyBills.Views.BillsIndex = Backbone.View.extend({
   initialize: function(){
   },
 
+  events: {
+    "click button.add-bill": "showAddBillButton",
+    "click button.create-bill": "addBill"
+  },
+
   render: function(){
     var content = this.template({ bills: this.collection });
     this.$el.html(content);
     return this;
+  },
+  showAddBillButton: function(event){
+    event.preventDefault();
+
+    $(".add-bill").toggleClass("hidden");
+
+                     
+  },
+  addBill: function(event) {
+    event.preventDefault();
+
+    $(".add-bill").toggleClass("hidden");
   }
 
   
