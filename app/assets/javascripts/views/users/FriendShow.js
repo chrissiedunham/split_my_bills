9a825fc7bc6706cdc376f3c480408b3f229bd28e
Chrissie Deist <chrissie.deist@gmail.com>
2014-03-31 11:13:56 -0700

    Rever show page back to not show paidUnpaid

--- app/assets/javascripts/views/users/FriendShow.js
+++ app/assets/javascripts/views/users/FriendShow.js
@@ -2,19 +2,36 @@ window.SplitMyBills.Views.FriendShow = Backbone.CompositeView.extend({
 
   template: JST["users/friend_show"],
 
-  initialize: function(options){
+  initialize: function(){
 
     this.listenTo(this.model, "sync change", this.render);
-    this.listenTo(this.model.debtorsBills(), "add change remove", this.render);
-   },
+    this.listenTo(this.model.bills(), "add change remove", this.render);
+    this.listenTo(this.model.relevantBills(), "add", this.render);
+    this.listenTo(this.model.relevantBills(), "change", this.render);
+    this.listenTo(this.model.relevantBills(), "remove", this.render);
+
+    this.addrelevantBillsIndexView();
+  },
+
+  events: {
+          
+          
+  },
+
+  addrelevantBillsIndexView: function() {
+    var indexView = new SplitMyBills.Views.BillsIndex( { 
+      collection: this.model.relevantBills(),
+      user: this.model
+    });
+    this.addSubview(".debtors-bills-index", indexView);
+    indexView.render();
+  },
 
   render: function(){
-    var content = this.template({ 
-      user: this.model, 
-      debtorsBills: this.model.debtorsBills() 
-    });
+    var content = this.template({ user: this.model });
     this.$el.html(content);
 
+    this.renderSubviews();
     return this;
   },
 })
